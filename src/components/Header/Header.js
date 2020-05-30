86ebbc560a9c437190abbae79f33a422e1fa93f3
Joshua <joshuakirwin@gmail.com>
2020-05-30 15:25:58 -0500

    Initial commit.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+//React core
+import React from "react";
+//CSS
+import "./header.scss";
+
+function Header() {
+  return (
+    <header>
+      <div className="app__header">
+        <a href="" className="app__header-link">
+          Blog
+        </a>
+        <a href="" className="app__header-link">
+          Nourish
+        </a>
+        <a href="" className="app__header-link">
+          Shop
+        </a>
+      </div>
+      <div className="app__header-subheader">
+        <a href="" className="app__header-subheader-link">
+          Excercise
+        </a>
+        <a href="" className="app__header-subheader-link">
+          Nutrition
+        </a>
+        <a href="" className="app__header-subheader-link">
+          Activity
+        </a>
+        <a href="" className="app__header-subheader-link">
+          Sleep
+        </a>
+        <a href="" className="app__header-subheader-button">
+          Sign Up
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
