0df437c34daecf56bea0d304fb9bc6a123f1ed44
Joshua <joshuakirwin@gmail.com>
2020-05-31 15:26:59 -0500

    preliminary fixed header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,18 +11,17 @@ const Header = (props) => {
   const PRIMARYNAV = headerNavItems["primary-navigation"];
   const SECONDARYNAV = headerNavItems["secondary-navigation"];
   return (
-    <header>
-      <nav
-        aria-label="Primary"
-        className={`app__header${
-          props.headerScroll ? " app__header--scroll" : ""
-        }`}
-      >
-        <ul className="app__header-list">
+    <header
+      className={`app__header${
+        props.headerScroll ? " app__header--scroll" : ""
+      }`}
+    >
+      <nav aria-label="Primary" className={"app__header-nav"}>
+        <ul className="app__header-nav-list">
           {PRIMARYNAV.map((item) => {
             return (
-              <li key={item.link} className="app__header-list-item">
-                <a href={`/${item.link}`} className="app__header-link">
+              <li key={item.link} className="app__header-nav-list-item">
+                <a href={`/${item.link}`} className="app__header-nav-link">
                   {item[`text-${props.lang}`]}
                 </a>
               </li>
@@ -33,27 +32,30 @@ const Header = (props) => {
       </nav>
       <nav
         aria-label="Secondary"
-        className={`app__header-subheader${
+        className={`app__header-nav-subheader${
           !props.mobileMenuToggled && props.isMobile
-            ? " app__header-subheader--hidden"
+            ? " app__header-nav-subheader--hidden"
             : ""
         }`}
       >
-        <ul className="app__header-subheader-list">
+        <ul className="app__header-nav-subheader-list">
           {SECONDARYNAV.map((item) => {
             return (
-              <li className="app__header-subheader-list-item" key={item.link}>
+              <li
+                className="app__header-nav-subheader-list-item"
+                key={item.link}
+              >
                 <a
                   href={`/${item.link}`}
-                  className="app__header-subheader-link"
+                  className="app__header-nav-subheader-link"
                 >
                   {item[`text-${props.lang}`]}
                 </a>
               </li>
             );
           })}
-          <li className="app__header-subheader-list-item">
-            <a className="app__header-subheader-button" href="/sign-up">
+          <li className="app__header-nav-subheader-list-item">
+            <a className="app__header-nav-subheader-button" href="/sign-up">
               Sign Up
             </a>
           </li>
