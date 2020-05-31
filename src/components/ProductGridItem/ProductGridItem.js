d66d05ed8ed3deec7eaec26511114a614e02d863
Joshua <joshuakirwin@gmail.com>
2020-05-31 17:45:54 -0500

    mucho grande style fixaroonis.

--- src/components/ProductGridItem/ProductGridItem.js
+++ src/components/ProductGridItem/ProductGridItem.js
@@ -7,10 +7,12 @@ function ProductGridItem(props) {
   const logoImg = props.img ? props.img : "";
   return (
     <div className="app__product-grid-item">
-      <img
-        src={require(`../../assets/images/product-grid-item-images/${logoImg}`)}
-        alt=""
-      />
+      <a href={props.link}>
+        <img
+          src={require(`../../assets/images/product-grid-item-images/${logoImg}`)}
+          alt={props.alt}
+        />
+      </a>
       <p className="app__product-grid-item-text">
         {props[`text-${props.lang}`]}
       </p>
