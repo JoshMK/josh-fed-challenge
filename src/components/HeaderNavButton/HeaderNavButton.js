185f58009f803da42d7264f974cad31ee4919f76
Joshua <joshuakirwin@gmail.com>
2020-06-01 10:08:56 -0500

    mobile menu nav button styles.

--- src/components/HeaderNavButton/HeaderNavButton.js
+++ src/components/HeaderNavButton/HeaderNavButton.js
@@ -11,9 +11,28 @@ const NavMenuButton = (props) => {
       onClick={props.toggleMobileMenu}
     >
       <span className="app__sr-only">Menu</span>
-      <span className="app__header-nav-button-line" aria-hidden="true"></span>
-      <span className="app__header-nav-button-line" aria-hidden="true"></span>
-      <span className="app__header-nav-button-line" aria-hidden="true"></span>
+      <span
+        className={`app__header-nav-button-line${
+          props.mobileMenuToggled
+            ? " app__header-nav-button-line--open app__header-nav-button-line--left"
+            : ""
+        }`}
+        aria-hidden="true"
+      ></span>
+      <span
+        className={`app__header-nav-button-line${
+          props.mobileMenuToggled ? " app__header-nav-button-line--open" : ""
+        }`}
+        aria-hidden="true"
+      ></span>
+      <span
+        className={`app__header-nav-button-line${
+          props.mobileMenuToggled
+            ? " app__header-nav-button-line--open app__header-nav-button-line--right"
+            : ""
+        }`}
+        aria-hidden="true"
+      ></span>
     </button>
   );
 };
