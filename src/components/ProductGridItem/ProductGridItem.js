1db7edff29696d1a1d71b2d4dfe0710354740495
Joshua <joshuakirwin@gmail.com>
2020-05-31 21:03:22 -0500

    semantic html + style fixes.

--- src/components/ProductGridItem/ProductGridItem.js
+++ src/components/ProductGridItem/ProductGridItem.js
@@ -7,15 +7,18 @@ const ProductGridItem = (props) => {
   const logoImg = props.img ? props.img : "";
   return (
     <div className="app__product-grid-item">
-      <a href={props.link}>
-        <img
-          src={require(`../../assets/images/product-grid-item-images/${logoImg}`)}
-          alt={props.alt}
-        />
-      </a>
-      <p className="app__product-grid-item-text">
-        {props[`text-${props.lang}`]}
-      </p>
+      <figure>
+        <a href={props.link}>
+          <img
+            className="app__product-grid-item-image"
+            src={require(`../../assets/images/product-grid-item-images/${logoImg}`)}
+            alt={props.alt}
+          />
+        </a>
+        <figcaption className="app__product-grid-item-text">
+          {props[`text-${props.lang}`]}
+        </figcaption>
+      </figure>
     </div>
   );
 };
